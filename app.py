d75e3fb7a2b1fb081a0efd19a10953cd6d847698
afattahi1387 <afattahi1387@gmail.com>
2022-09-06 21:45:49 +0430

    added writers table and writer_id field to articles table. added get_single_article and get_single_writer method and single article page.

--- app.py
+++ app.py
@@ -22,10 +22,20 @@ def create_tables_in_database():
             `id` INT(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
             `name` VARCHAR(255) NOT NULL,
             `image` VARCHAR(255) NOT NULL,
+            `writer_id` INT(255) NOT NULL,
             `short_description` TEXT NOT NULL,
             `long_description` TEXT NOT NULL
         ) ENGINE InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
     ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS `writers` (
+            `id` INT(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            `name` VARCHAR(255) NOT NULL,
+            `image` VARCHAR(255) NOT NULL,
+            `description` TEXT NOT NULL
+        ) ENGINE InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+    ''');
 
 def close_database():
     db_connection.commit()
@@ -45,6 +55,22 @@ def get_all_articles(orderby):
     cursor.execute(query)
     return cursor.fetchall()
 
+def get_single_article(articleID):
+    cursor.execute(f"SELECT * FROM articles WHERE id = '{articleID}'")
+    article = cursor.fetchone()
+    if not article:
+        return False
+    
+    return article
+
+def get_single_writer(writerID):
+    cursor.execute(f"SELECT * FROM writers WHERE id = '{writerID}'")
+    writer = cursor.fetchone()
+    if not writer:
+        return False
+
+    return writer
+
 create_tables_in_database()
 
 @app.route('/')
@@ -52,6 +78,13 @@ def home():
     categories = get_all_categories(False)
     articles = get_all_articles(True)
     return render_template('home.html', icon = config.APP_ICON, title = config.APP_NAME, categories = categories, articles = articles)
+
+@app.route('/single-article/<int:id>')
+def single_article(id):
+    categories = get_all_categories(False)
+    article = get_single_article(id)
+    writer = get_single_writer(article[3])
+    return render_template('single_article.html', icon = config.APP_ICON, title = config.APP_NAME, categories = categories, article = article, writer = writer)
 
 if __name__ == '__main__':
     app.run(debug=True)
