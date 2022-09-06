d7a67db6738d4219d90d6a8f16935e0d89c88874
afattahi1387 <afattahi1387@gmail.com>
2022-09-06 08:26:15 +0430

    added connect to database and cursor to app.py file and create_tables_in_database function for create tables in database.

--- app.py
+++ app.py
@@ -5,9 +5,27 @@ from flask import Flask, render_template, url_for, Response
 
 app = Flask(__name__)
 
+db_connection = MySQLdb.connect(host = config.DB_HOST, user = config.DB_USERNAME, passwd = config.DB_PASSWORD, db = config.DB_NAME)
+
+cursor = db_connection.cursor()
+
+def create_tables_in_database():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS `articles` (
+            `id` INT(255) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            `name` VARCHAR(255) NOT NULL,
+            `short_description` TEXT NOT NULL,
+            `long_description` TEXT NOT NULL
+        ) ENGINE InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_persian_ci;
+    ''')
+
+create_tables_in_database()
+
 @app.route('/')
 def home():
     return render_template('home.html', icon = config.APP_ICON, title = config.APP_NAME)
 
 if __name__ == '__main__':
+    db_connection.commit()
+    db_connection.close()
     app.run(debug=True)
